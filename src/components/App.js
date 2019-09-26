4b8817c9ea2f7c7270f29491a26ad44c8124e150
Javiercerna <javier.cerna@pucp.pe>
2019-09-26 16:07:02 +0200

    Refactor getToken methods and move to api.js, w/ use of pify

--- src/components/App.js
+++ src/components/App.js
@@ -3,71 +3,30 @@ import './App.css';
 import TestState from '../context/test/TestState';
 import GoogleCalendar from './GoogleCalendar/GoogleCalendar';
 import StateReceiver from './StateReceiver';
-import getCalendarList from './GoogleCalendar/api';
-
-const CLIENT_ID = process.env.REACT_APP_GOOGLE_CALENDAR_CLIENT_ID;
-const API_KEY = process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY;
+import {
+  getCalendarList,
+  getTokenFromExtension,
+  isAppLoadedAsExtension,
+  getTokenFromWebApp
+} from './GoogleCalendar/api';
 
 function App() {
   const [token, setToken] = useState('');
 
-  const getTokenWebApp = () => {
-    let GoogleAuth;
-    window.gapi.client
-      .init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        scope: 'https://www.googleapis.com/auth/calendar.readonly',
-        discoveryDocs: [
-          'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'
-        ]
-      })
-      .then(() => {
-        GoogleAuth = window.gapi.auth2.getAuthInstance();
-
-        if (!GoogleAuth.isSignedIn.get()) {
-          window.gapi.auth2
-            .getAuthInstance()
-            .signIn()
-            .then(() => {
-              const newToken = GoogleAuth.currentUser.get().getAuthResponse()
-                .access_token;
-
-              if (newToken !== undefined) {
-                setToken(newToken);
-              } else {
-                console.log('Token is undefined');
-              }
-            });
-        } else {
-          const newToken = GoogleAuth.currentUser.get().getAuthResponse()
-            .access_token;
-
-          if (newToken !== undefined) {
-            setToken(newToken);
-          } else {
-            console.log('Token is undefined');
-          }
-        }
-      });
-  };
-
-  const getToken = () => {
-    if (window.chrome && window.chrome.runtime && window.chrome.runtime.id) {
-      window.chrome.identity.getAuthToken({ interactive: true }, function (
-        extensionToken
-      ) {
-        setToken(extensionToken);
-      });
+  const getToken = async () => {
+    if (isAppLoadedAsExtension()) {
+      const extensionToken = await getTokenFromExtension();
+      setToken(extensionToken);
     } else {
-      window.gapi.load('client:auth2', getTokenWebApp);
+      const webAppToken = await getTokenFromWebApp();
+      setToken(webAppToken);
     }
   };
 
   useEffect(() => {
     if (token !== '') {
-      console.log(token);
-      getCalendarList(API_KEY, token);
+      console.log(`New token: ${token}`);
+      getCalendarList(token);
     }
   }, [token]);
 
@@ -78,13 +37,7 @@ function App() {
       <div className="App">
         <h1>App goes here</h1>
         <a href={`https://google.com/search?q=${link}`}>Link to google</a>
-        <button
-          onClick={async () => {
-            getToken();
-          }}
-        >
-          Load calendar
-        </button>
+        <button onClick={getToken}>Load calendar</button>
       </div>
     </TestState>
   );
--- src/components/GoogleCalendar/api.js
+++ src/components/GoogleCalendar/api.js
@@ -1,16 +1,58 @@
 import axios from 'axios';
+import pify from 'pify';
 
-const getCalendarList = async (apiKey, token) => {
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CALENDAR_CLIENT_ID;
+const API_KEY = process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY;
+const DISCOVERY_DOCS = [
+  'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'
+];
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+
+export const isAppLoadedAsExtension = () => {
+  return window.chrome && window.chrome.runtime && window.chrome.runtime.id;
+};
+
+export const getTokenFromExtension = async () => {
+  const getAuthTokenPromisified = pify(window.chrome.identity.getAuthToken, {
+    errorFirst: false // Needed for Chrome Extensions APIs
+  });
+
+  const token = await getAuthTokenPromisified({ interactive: true });
+  return token;
+};
+
+export const getTokenFromWebApp = async () => {
+  const gapiLoadPromisified = pify(window.gapi.load, {
+    errorFirst: false // Needed for Chrome Extensions APIs
+  });
+
+  await gapiLoadPromisified('client:auth2');
+
+  await window.gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    scope: SCOPES,
+    discoveryDocs: DISCOVERY_DOCS
+  });
+
+  const GoogleAuth = window.gapi.auth2.getAuthInstance();
+
+  if (!GoogleAuth.isSignedIn.get()) {
+    await window.gapi.auth2.getAuthInstance().signIn();
+  }
+
+  return GoogleAuth.currentUser.get().getAuthResponse().access_token;
+};
+
+export const getCalendarList = async token => {
   const headers = {
     Authorization: `Bearer ${token}`,
     Accept: 'application/json'
   };
   const timeMin = new Date().toISOString();
-  const url = `https://www.googleapis.com/calendar/v3/calendars/primary/events?key=${apiKey}&timeMin=${timeMin}`;
+  const url = `https://www.googleapis.com/calendar/v3/calendars/primary/events?key=${API_KEY}&timeMin=${timeMin}`;
 
   const response = await axios.get(url, { headers });
   console.log(response);
   return response;
 };
-
-export default getCalendarList;
