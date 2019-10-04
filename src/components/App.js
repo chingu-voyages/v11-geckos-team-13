caab8c057cbe587c23cda1f273419ab7a436ec2b
Javiercerna <javier.cerna@pucp.pe>
2019-10-04 18:11:53 +0200

    Hide React context components (for now), remove google search

--- src/components/App.js
+++ src/components/App.js
@@ -16,19 +16,18 @@ const WeatherKey = process.env.REACT_APP_WEATHER_API_KEY;
 const IpKey = process.env.REACT_APP_IP_KEY;
 
 function App() {
-  const link = 'hello internet';
   return (
     <div>
       <img src={banner} alt="banner" className="App-banner" />
       <Search className="App-Search" />
       <div className="App">
-        <h1>App goes here</h1>
-        <a href={`https://google.com/search?q=${link}`}>Link to google</a>
         <GoogleCalendar />
       </div>
-      <TestState>
-        <StateReceiver />
-      </TestState>
+      <div className="App-react-context">
+        <TestState>
+          <StateReceiver />
+        </TestState>
+      </div>
       <CurrencyConverter API_KEY={CurrencyKey} />
       <Weather IP_KEY={IpKey} API_KEY={WeatherKey} />
     </div>
